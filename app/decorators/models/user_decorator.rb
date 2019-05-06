b5945d9168166a74f8e783f4a3cc5ce3d099026b
Maxim Khan-Magomedov <maxim.km@gmail.com>
2019-05-06 23:54:10 +0300

    Puma bunstubs, cast surname to string

--- app/decorators/models/user_decorator.rb
+++ app/decorators/models/user_decorator.rb
@@ -1,7 +1,7 @@
 User.class_eval do
   def profile_name
     profile = data['profile'].to_h
-    result = "#{profile['name']} #{profile['surname'].upcase}"
+    result = "#{profile['name']} #{profile['surname'].to_s.upcase}"
     result.blank? ? screen_name : result
   end
 end
